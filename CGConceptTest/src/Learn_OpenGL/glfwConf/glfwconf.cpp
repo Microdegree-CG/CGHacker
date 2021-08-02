c3fbdbd90ef967f6ccdfc489551d0db1106a8ef6
guoliang wang <tswanggl@hotmail.com>
2021-08-02 11:24:55 +0800

    Add some comment to GLFW usage

--- CGConceptTest/src/Learn_OpenGL/glfwConf/glfwconf.cpp
+++ CGConceptTest/src/Learn_OpenGL/glfwConf/glfwconf.cpp
@@ -6,17 +6,19 @@ int main()
 
 	if (!glfwInit())
 		return -1;
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "Learn OpenGL", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
 		return -1;
 	}
 
+	// Tell GLFW to make the context of our window the main context on the current thread.
 	glfwMakeContextCurrent(window);
 	while (!glfwWindowShouldClose(window))
 	{
