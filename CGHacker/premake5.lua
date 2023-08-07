1a0e1c112e67cefd64a62bb840441758d620bf80
Guoliang WANG <tswanggl@gmail.com>
2023-08-07 23:23:44 +0800

    update

--- CGHacker/premake5.lua
+++ CGHacker/premake5.lua
@@ -1,77 +1,64 @@
 project "CGHacker"
-    kind "StaticLib"
-    language "C++"
-    cppdialect "C++17"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
 
+	targetdir ("../bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("../bin-int/" .. outputdir .. "/%{prj.name}")
 
-    targetdir ("..bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("..bin-int/" .. outputdir .. "/%{prj.name}")
+	pchheader "glpch.h"
+	pchsource "src/glpch.cpp"
 
--- not use precompiled header
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"vendor/stb_image/**.h",
+		"vendor/stb_image/**.cpp",
+		"vendor/glm/glm/**.hpp",
+		"vendor/glm/glm/**.inl",
+	}
 
-    pchheader "cgpch.h"
-    pchsource "%{wks.location}/CGHacker/src/cgpch.cpp"
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
 
-    files 
-    {
-        "%{wks.location}/CGHacker/src/CGHacker/**.h",
-        "%{wks.location}/CGHacker/src/CGHacker/**.cpp",
-        "%{wks.location}/CGHacker/src/cg.h",
-        "%{wks.location}/CGHacker/src/cgpch.h",
-        "%{wks.location}/CGHacker/src/cgpch.cpp",
-    }
+	includedirs
+	{
+		"src",
+		"vendor/spdlog/include",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.GLAD}",
+		"%{IncludeDir.IMGUI}",
+		"%{IncludeDir.GLM}",
+		"%{IncludeDir.STB}"
+	}
 
+	links 
+	{ 
+		"GLFW",
+		"Glad",
+		"ImGui",
+		"opengl32.lib"
+	}
 
-    defines
-    {
-        "_CRT_SECURE_NO_WARNINGS",
-        "GLFW_INCLUDE_NONE",
-    }
+	filter "system:windows"
+		systemversion "latest"
 
-    includedirs
-    {
-        "src", 
-        "vendor/spdlog/include",
-        "vendor/eigen/Eigen",
-        "%{IncludeDir.GLFW}",
-        "%{IncludeDir.GLAD}",
-        "%{IncludeDir.STB}",
-    }
+		defines
+		{
+			"GLCORE_PLATFORM_WINDOWS",
+			"GLFW_INCLUDE_NONE"
+		}
 
-    links 
-    {
-        "GLFW",
-        "opengl32.lib"
-    }
+	filter "configurations:Debug"
+		defines "GLCORE_DEBUG"
+		runtime "Debug"
+		symbols "on"
 
-    libdirs
-    {
-    }
-
-    --filter "files:**.c"
-    --    flags {"NoPCH"}
-
-    filter "system:windows"
-        systemversion "latest"
-
-        defines 
-        {
-        }
-
-    filter "configurations:Debug"
-        defines "CG_DEBUG"
-        runtime "Debug"
-        symbols "on"
-        staticruntime "off"
-
-    filter "configurations:Release"
-        defines "CG_RELEASE"
-        runtime "Release"
-        optimize "on"
-        staticruntime "on"
-
-    filter "configurations:Dist"
-        defines "CG_DIST"
-        runtime "Release"
-        optimize "on"
-        staticruntime "on"
+	filter "configurations:Release"
+		defines "GLCORE_RELEASE"
+		runtime "Release"
+		optimize "on"
